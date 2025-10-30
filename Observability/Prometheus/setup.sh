fbbfa453f755ce5c90664c0d1a644ecff68c4398
Flavio Venturini <flavio185@hotmail.com>
2025-10-30 01:19:29 -0300

    add kserve monitoring

--- Observability/Prometheus/setup.sh
+++ Observability/Prometheus/setup.sh
@@ -6,7 +6,7 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install prometheus-stack prometheus-community/kube-prometheus-stack \
-  --namespace observability-ns \
-  --create-namespace
+kubectl create namespace monitoring
 
+helm install prometheus-operator prometheus-community/kube-prometheus-stack \
+  --namespace monitoring
--- setup.sh
+++ setup.sh
@@ -27,3 +27,5 @@ sleep 30
 # Create AWS credentials secret in the app namespace
 # get aws credentials from .env file
 source .env
+
+app-ns/setup-app-ns.sh
